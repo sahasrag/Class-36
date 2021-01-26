700a57603229aa5374bb6a1c5ebb3f206c0fcd2a
sahasrag <71852686+sahasrag@users.noreply.github.com>
2021-01-26 16:41:41 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class-36
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database,player, form, game, playerCount;
+var gameState = 0;
+var allPlayers;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+   if(playerCount===4){
+       game.update(1);
+   }
+   if(gameState===1){
+       clear();
+       game.play();
+   }
+}
+
